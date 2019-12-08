88bd1ada825e881fdf14575bbd00fdabda11638e
unknown <pcaudilla@gmail.com>
2019-12-08 13:51:57 -0800

    updating homework

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -6,32 +6,33 @@ import scrape_mars
 app = Flask(__name__) 
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
 
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
-def home():
+def index():
 
     # Find one record of data from the mongo database
-    mars_news = mongo.db.collection.find_one()
+    mars_data = mongo.db.mars_data.find_one()
 
     # Return template and data
-    return render_template("index.html", news=mars_news)
+    return render_template("index.html", news=mars_data)
 
 
 # Route that will trigger the scrape function
 @app.route("/scrape")
-def scrape():
+def scraper():
 
-    # Run the scrape function
-    mars_data = scrape_mars.scrape_info()
+    mars_data = mongo.db.mars_data
+    headline = scrape_mars.scrape()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, mars_data, upsert=True)
+    mars_data.update({}, headline, upsert=True)
 
     # Redirect back to home page
-    return redirect("/")
+    return redirect("/", code=302)
 
 
 if __name__ == "__main__":
--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -13,6 +13,7 @@ def init_browser():
 
 def scrape():
     browser = init_browser()
+    mars_data = {}
 
     # Get News Title and News P
     url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
@@ -23,41 +24,46 @@ def scrape():
 
     title = soup.find('div', class_='content_title').text
     excerpt = soup.find('div', class_='rollover_description_inner').text
-    news_title = title.replace('\n', '')
-    news_p = excerpt.replace('\n', '')
+    mars_data["news_title"] = title.replace('\n', '')
+    mars_data["news_p"] = excerpt.replace('\n', '')
 
     # Get Featured URL
-    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
-    html = browser.html
-    soup = bs(html, "html.parser") 
+    #url2 = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    #html2 = browser(url2)
+    #soup = bs(html2, "html.parser") 
 
-    link = soup.find('div', class_='default floating_text_area ms-layer')
-    image_url = link.a.attrs['data-fancybox-href']
-    featured_image_url = 'https://www.jpl.nasa.gov/' + image_url
+    #link = soup.find('div', class_='default floating_text_area ms-layer')
+    #image_url = link.a.attrs['data-fancybox-href']
+    #mars_data["featured_image_url"] = 'https://www.jpl.nasa.gov/' + image_url
 
-    # Get Mars weather
-    url = 'https://twitter.com/marswxreport?lang=en'
-    html = browser.html
-    soup = bs(html, "html.parser")
+    #  Get Mars weather
+    #url = 'https://twitter.com/marswxreport?lang=en'
+    #html = browser.html
+    #soup = bs(html, "html.parser")
 
-    marsweather = soup.find('p', class_="TweetTextSize TweetTextSize--normal js-tweet-text tweet-text")
-    mars_weather = marsweather.text
+    #marsweather = soup.find('p', class_="TweetTextSize TweetTextSize--normal js-tweet-text tweet-text")
+    #mars_weather = marsweather.text
 
-    # Get Hemisphere images
-    url = 'https://astrogeology.usgs.gov/search/map/Mars/Viking/cerberus_enhanced'
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
+     #  Get Hemisphere images
+    #url = 'https://astrogeology.usgs.gov/search/map/Mars/Viking/cerberus_enhanced'
+    #response = requests.get(url)
+    #soup = BeautifulSoup(response.text, 'lxml')
 
-    # Store data in a dictionary
-    mars_data = {"news_title": news_title,
-        "news_p": news_p,
-        "featured_image_url": featured_image_url,
-        "mars_weather": max_temp}
+     #  Store data in a dictionary
+    #mars_data = {"news_title": news_title,
+    #    "news_p": news_p,
+    #    "featured_image_url": featured_image_url,
+    #    "mars_weather": max_temp}
 
-    # Close the browser after scraping
-    browser.quit()
+     #  Close the browser after scraping
+    #browser.quit()
 
     # Return results
     return mars_data
+
+if __name__ == "__main__":
+    print(scrape())
+
+
 
 
